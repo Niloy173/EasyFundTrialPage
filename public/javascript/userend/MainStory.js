d531b68dc1f4ff33332bdb218d049f2eb6f467a4
unknown <niloysaha173@gmail.com>
2022-05-27 13:52:10 +0600

    added functionality on toggle based on payment

--- public/javascript/userend/MainStory.js
+++ public/javascript/userend/MainStory.js
@@ -21,7 +21,43 @@ const interval = setInterval(function () {
 window.setTimeout(function(){
 
   document.getElementById("mainStory").style.height = document.getElementById("mainStory").scrollHeight;
+
  })
 
 
  
+ /* this function is used for copy url which is already inside the input box */
+ function CopyLink()
+ {
+   const checker =  document.getElementById("copy-btn").innerHTML.toLowerCase.toString();
+
+   let link_box = document.getElementById("project-link");
+   link_box.select();
+   navigator.clipboard.writeText(link_box.value);
+
+   if(checker !="copied")
+   {
+     document.getElementById("copy-btn").innerHTML = "copied"
+   }
+ 
+  
+
+ }
+
+
+ /* this functionality determines how much amount the selected project already gained */
+
+ const target_amount = parseInt(document.getElementById("goal").innerHTML);
+//  console.log(target_amount)// 1100
+ const curr_amount = document.getElementById("curr_amount").innerHTML;
+//  console.log(curr_amount) // 560
+
+ const Getting_percentage = ( curr_amount / target_amount ) * 100;
+  document.getElementById("progress").value += Getting_percentage;
+
+//  console.log(Getting_percentage);
+
+ /*---------------------------------*/
+
+
+
